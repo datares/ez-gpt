4756aa953fb7fbb628e753643f7b3d7c2e3f8573
Colin Curtis <curtisc@pictor-labs.com>
2021-11-15 11:04:36 -0800

    add text generation

---
+++ generate.py
@@ -0,0 +1,12 @@
+from config import config
+from model import Model
+
+
+def main():
+    ckpt_path = config["checkpoint_path"]
+    model = Model().load_from_checkpoint(ckpt_path)
+    for i in range(1):
+        model.generate()
+
+if __name__ == "__main__":
+    main()
--- tokenizer.py
+++ tokenizer.py
@@ -1,2 +1,3 @@
 from transformers import GPT2Tokenizer
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2', bos_token='<|startoftext|>', eos_token='<|endoftext|>', pad_token='<|pad|>')
+tokenizer.pad_token = tokenizer.eos_token
